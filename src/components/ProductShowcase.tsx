a0e63afd3b8050b2f75d71853f5f44437e247659
nepidkhadka <dipenkhadka191@gmail.com>
2024-07-19 17:03:40 +0545

    ProductShowcase Section Completed

--- src/components/ProductShowcase.tsx
+++ src/components/ProductShowcase.tsx
@@ -1,5 +1,14 @@
+import Image from "next/image";
 import appScreen from "../assets/images/app-screen.png";
 
 export const ProductShowcase = () => {
-  return null;
+  return (
+    <div className="bg-black text-white bg-gradient-to-b from-black to-[#5D2CAB] py-[72px] sm:py-24">
+      <div className="container">
+          <h2 className="text-center text-5xl sm:text-6xl font-bold tracking-tighter" >Intuitive Interface</h2>
+          <p className="text-xl text-center text-white/70 mt-5 max-w-xl mx-auto">Celebrate the joy of accomplishment with an app designed to track your progress.</p>
+          <Image className="mt-14" src={appScreen} alt="Product" />
+      </div>
+    </div>
+  );
 };
