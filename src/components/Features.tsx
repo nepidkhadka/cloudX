b69354cc28998a326baaff11a5dcd28e3d73a308
nepidkhadka <dipenkhadka191@gmail.com>
2024-07-19 16:53:02 +0545

    Features Section Completed

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,3 +1,5 @@
+import EcoSystemIcon from "@/assets/icons/ecosystem.svg"
+
 const features = [
   {
     title: "Integration Ecosystem",
@@ -17,5 +19,27 @@ const features = [
 ];
 
 export const Features = () => {
-  return null;
+  return (
+    <div className="bg-black text-white py-[72px] sm:py-24">
+      <div className="container">
+        <div>
+          <h2 className="text-center font-bold text-5xl sm:text-6xl tracking-tighter">Everything You Need</h2>
+          <p className="text-center mt-5 text-xl text-white/70 max-w-xl mx-auto">Enjoy Customizable lists, team works tools, and smart tracking all in one place. Set tasks, get reminders, and see your progress simply and quickly</p>
+        </div>
+        <div className="mt-16 flex flex-col sm:flex-row gap-4 ">
+          {
+            features.map(({ title, description }) => (
+              <div key={title} className="border border-white/30 px-5 py-10 text-center rounded-lg sm:flex-1">
+                <div className="inline-flex h-14 w-14 bg-white text-black justify-center items-center rounded-lg">
+                  <EcoSystemIcon />
+                </div>
+                <h3 className="mt-6 font-bold" >{title}</h3>
+                <p className="mt-2 text-white/70" >{description}</p>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </div>
+  );
 };
